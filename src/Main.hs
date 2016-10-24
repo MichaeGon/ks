ce0c9be9f059cf1060edf4ed05ce790f6a64fac0
MichaeGon <lodnix@gmail.com>
2016-10-24 13:13:09 +0900

    --prefix=

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,30 @@
+import Data.List
+import Data.Maybe
 import System.Directory
 import System.FilePath
+import System.Environment
 
 main :: IO ()
-main = prepare >>= main'
+main = getArgs >>= prepare . parse >>= main'
     where
         main' p = readFile "ks.hs" >>= writeFile p
             >> getPermissions p >>= setPermissions p . setOwnerExecutable True
 
-prepare :: IO FilePath
+prepare :: Maybe FilePath -> IO FilePath
+prepare p = getHomeDirectory >>= prepare' . flip fromMaybe p . defpf
+    where
+        defpf x = x </> ".local" </> "bin"
+        prepare' x = (x </> "ks") <$ createDirectoryIfMissing True x 
+{-
 prepare = getHomeDirectory >>= prepare'
     where
         prepare' p = (dstdir </> "ks") <$ createDirectoryIfMissing True dstdir
             where
                 dstdir = p </> ".local" </> "bin"
+-}
+
+parse :: [String] -> Maybe FilePath
+parse (x : xs)
+    | "--prefix=" `isPrefixOf` x = return . drop 1 $ dropWhile (/= '=') x
+    | otherwise = parse xs
+parse _ = Nothing
